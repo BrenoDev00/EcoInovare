b10ed833147f84149a2d579da581d48c652f28f6
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-06-05 12:47:08 -0300

    atualização

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const 
-})
-// Seção Formulário de Conversão
+document.addEventListener("DOMContentLoaded", function () {
+  // Seção Formulário de Conversão
+  const conversionForm = document.getElementById("conversion-form");
+  const requiredFields = document.querySelectorAll("error-message");
+
+  conversionForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    validateNameField();
+  });
+
+  function validateNameField() {
+    requiredFields.addEventListener("input", function () {
+      if (requiredFields[0].value.length < 3) {
+        requiredFields[0].style.display = "flex";
+      } else {
+        requiredFields[0].style.display = "none";
+      }
+    });
+  }
+});
