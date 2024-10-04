534588247d5c7f8831e005d82dbd4c2f5e672291
Breno <brenosilvagoncalves12@gmail.com>
2024-10-04 14:05:42 -0300

    Tag noscript adicionada e função toggleMenu() adicionada no arquivo about-us.js

--- ASSETS/JS/about-us.js
+++ ASSETS/JS/about-us.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  // Menu de Navegação
+  const menuBtn = document.querySelector(".btn-menu");
+  const navigationItems = document.querySelector(".navigation-items");
+
+  function toggleMenu() {
+    menuBtn.addEventListener("click", function () {
+      navigationItems.classList.toggle("navigation-open");
+
+      if (navigationItems.classList.contains("navigation-open")) {
+        menuBtn.src = "ASSETS/IMAGES/HOME/icone-fechar-menu-hamburguer.svg";
+      } else {
+        menuBtn.src = "ASSETS/IMAGES/HOME/icone-menu-hamburguer.svg";
+      }
+    });
+  }
+
+  toggleMenu();
+});
