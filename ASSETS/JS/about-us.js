72f294d30a8c27d3d168f6375916687b63253e66
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-10-30 19:04:20 -0300

    criação do arquivo interactive-menu-functionality.js para alternar o menu de navegação de forma dinâmica através do javascript

--- ASSETS/JS/about-us.js
+++ ASSETS/JS/about-us.js
@@ -1,22 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Menu de Navegação
-  const menuBtn = document.querySelector(".btn-menu");
-  const navigationItems = document.querySelector(".navigation-items");
-
-  function toggleMenu() {
-    menuBtn.addEventListener("click", function () {
-      navigationItems.classList.toggle("navigation-open");
-
-      if (navigationItems.classList.contains("navigation-open")) {
-        menuBtn.src = "/ASSETS/IMAGES/HOME/icone-fechar-menu-hamburguer.svg";
-      } else {
-        menuBtn.src = "/ASSETS/IMAGES/HOME/icone-menu-hamburguer.svg";
-      }
-    });
-  }
-
-  toggleMenu();
-
   // Conteúdo Principal
   const aboutUsBtn = document.querySelectorAll(".btn-about-us");
   const borderBottom = document.querySelectorAll(".border-bottom");
--- ASSETS/JS/home.js
+++ ASSETS/JS/home.js
@@ -1,22 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Menu de Navegação
-  const menuBtn = document.querySelector(".btn-menu");
-  const navigationItems = document.querySelector(".navigation-items");
-
-  function toggleMenu() {
-    menuBtn.addEventListener("click", function () {
-      navigationItems.classList.toggle("navigation-open");
-
-      if (navigationItems.classList.contains("navigation-open")) {
-        menuBtn.src = "ASSETS/IMAGES/HOME/icone-fechar-menu-hamburguer.svg";
-      } else {
-        menuBtn.src = "ASSETS/IMAGES/HOME/icone-menu-hamburguer.svg";
-      }
-    });
-  }
-
-  toggleMenu();
-
   // Seção Depoimentos
   let count = 1;
   document.getElementById("radio-btn1").checked = true;
