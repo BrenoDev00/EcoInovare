6675c04fedd6cbc27f3eeec0f7c5bb01d920048b
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-11-11 19:15:22 -0300

    refatoração do código arquivo services.js e ajustes no modal de serviços na página de serviços

--- ASSETS/JS/services.js
+++ ASSETS/JS/services.js
@@ -5,55 +5,53 @@ document.addEventListener("DOMContentLoaded", function () {
   let serviceDescription = document.querySelector(".service-description");
   const btnCloseModal = document.querySelector(".btn-close-modal");
 
-  function teste() {
-    switch (clickedServiceButton) {
-      case 0:
-        serviceModal.showModal();
-        serviceName.textContent = "Certificação Ambiental";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Assistência na
-                obtenção de certificações ambientais reconhecidas
-                internacionalmente, como ISO 14001, LEED (Liderança em Energia e
-                Design Ambiental) e outras certificações específicas da indústria.`;
-        break;
-
-      case 1:
-        serviceName.textContent = "Compensação de Carbono";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Um serviço voltado para 
-          compensar as emissões de carbono das empresas por meio de 
-          iniciativas como o plantio de árvores ou apoio a projetos de energia limpa.`;
-        break;
-
-      case 2:
-        serviceName.textContent = "Gestão de Resíduos";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Desenvolvimento e implementação
-           de programas de gestão de resíduos, incluindo avaliação de resíduos, otimização de
-          processos e educação da equipe sobre práticas de reciclagem.`;
-        break;
-
-      case 3:
-        serviceName.textContent = "Eficiência Energética";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Soluções para reduzir o
-           consumo de energia, como auditorias energéticas, implementação de sistemas de iluminação e 
-           recomendação de fontes renováveis.`;
-        break;
-
-      case 4:
-        serviceName.textContent = "Consultoria em Sustentabilidade";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Ajudar empresas a desenvolver e
-           implementar estratégias sustentáveis personalizadas, incluindo avaliação de pegada de carbono, desenvolvimento 
-           de planos de sustentabilidade e treinamento em equipe.`;
-        break;
-
-      case 5:
-        serviceName.textContent = "Monitoramento Sustentável";
-        serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>:Realizamos monitoramento contínuo
-           das práticas sustentáveis e do impacto ambiental das operações da empresa. Através de relatórios detalhados e 
-           dashboards personalizados, ajudamos a acompanhar o consumo de recursos, emissões de carbono e eficiência energética. `;
-    }
-  }
-
   for (let i = 0; i < btnServiceCard.length; i++) {
     btnServiceCard[i].addEventListener("click", function () {
+      switch (i) {
+        case 0:
+          serviceName.textContent = "Certificação Ambiental";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Assistência na
+                  obtenção de certificações ambientais reconhecidas
+                  internacionalmente, como ISO 14001, LEED (Liderança em Energia e
+                  Design Ambiental) e outras certificações específicas da indústria.`;
+          break;
+
+        case 1:
+          serviceName.textContent = "Compensação de Carbono";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Um serviço voltado para
+            compensar as emissões de carbono das empresas por meio de
+            iniciativas como o plantio de árvores ou apoio a projetos de energia limpa.`;
+          break;
+
+        case 2:
+          serviceName.textContent = "Gestão de Resíduos";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Desenvolvimento e implementação
+             de programas de gestão de resíduos, incluindo avaliação de resíduos, otimização de
+            processos e educação da equipe sobre práticas de reciclagem.`;
+          break;
+
+        case 3:
+          serviceName.textContent = "Eficiência Energética";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Soluções para reduzir o
+             consumo de energia, como auditorias energéticas, implementação de sistemas de iluminação e
+             recomendação de fontes renováveis.`;
+          break;
+
+        case 4:
+          serviceName.textContent = "Consultoria em Sustentabilidade";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Ajudar empresas a desenvolver e
+             implementar estratégias sustentáveis personalizadas, incluindo avaliação de pegada de carbono, desenvolvimento
+             de planos de sustentabilidade e treinamento em equipe.`;
+          break;
+
+        case 5:
+          serviceName.textContent = "Monitoramento Sustentável";
+          serviceDescription.innerHTML = `<span class="text-highlight">Descrição</span>: Realizamos monitoramento contínuo
+             das práticas sustentáveis e do impacto ambiental das operações da empresa. Através de relatórios detalhados e
+             dashboards personalizados, ajudamos a acompanhar o consumo de recursos, emissões de carbono e eficiência energética. `;
+          break;
+      }
+
       serviceModal.showModal();
     });
   }
